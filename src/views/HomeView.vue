6c9937094b1d5976fbcd75617352f207da412dd9
hanool <29415530+hanool@users.noreply.github.com>
2023-10-26 20:59:06 +0900

    add HomeView with title image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>
-    <p>hello, world</p>
-  </main>
+	<main>
+		<div>
+			<header class="main-header">
+				<MainHeaderSVG class="main-header-svg" />
+			</header>
+		</div>
+	</main>
 </template>
+
+<script setup lang="ts">
+import MainHeaderSVG from '@/components/header/MainHeaderSVG.vue'
+</script>
+
+<style scoped>
+.main-header {
+	margin: auto;
+	width: 80%;
+	min-height: 500px;
+	background-image: url('@/assets/header.jpg');
+	background-position: 50% 50%;
+	background-repeat: no-repeat;
+}
+
+header {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
